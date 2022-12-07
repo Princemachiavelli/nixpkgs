ed3f09569fb9df5c3d8d519fe08b3b0ce5c58432
Josh Hoffer <jhoffer@sansorgan.es>
2022-12-07 00:24:18 -0700

    Revert "ruby: trying to fix issue building 3.1.3"
    This reverts commit c3018eb537b5b5febbd78583e0fb5343f0bbaf28.

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -2,16 +2,16 @@
 
 {
   "2.7.7" = ops useRailsExpress [
-    #"${patchSet}/patches/ruby/2.7/head/railsexpress/01-fix-with-openssl-dir-option.patch"
-    "${patchSet}/patches/ruby/2.7/head/railsexpress/01-fix-broken-tests-caused-by-ad.patch"
-    "${patchSet}/patches/ruby/2.7/head/railsexpress/02-improve-gc-stats.patch"
-    "${patchSet}/patches/ruby/2.7/head/railsexpress/03-more-detailed-stacktrace.patch"
-    "${patchSet}/patches/ruby/2.7/head/railsexpress/04-malloc-trim.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/01-fix-with-openssl-dir-option.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/02-fix-broken-tests-caused-by-ad.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/03-improve-gc-stats.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/04-more-detailed-stacktrace.patch"
+    "${patchSet}/patches/ruby/2.7/head/railsexpress/05-malloc-trim.patch"
   ];
   "3.0.5" = ops useRailsExpress [
-    #"${patchSet}/patches/ruby/3.0/head/railsexpress/01-fix-with-openssl-dir-option.patch"
-    "${patchSet}/patches/ruby/3.0/head/railsexpress/01-improve-gc-stats.patch"
-    "${patchSet}/patches/ruby/3.0/head/railsexpress/02-malloc-trim.patch"
+    "${patchSet}/patches/ruby/3.0/head/railsexpress/01-fix-with-openssl-dir-option.patch"
+    "${patchSet}/patches/ruby/3.0/head/railsexpress/02-improve-gc-stats.patch"
+    "${patchSet}/patches/ruby/3.0/head/railsexpress/03-malloc-trim.patch"
   ];
   "3.1.3" = ops useRailsExpress [
     "${patchSet}/patches/ruby/3.1/head/railsexpress/01-improve-gc-stats.patch"
